1f4d3ec6b99c7f9a1f66f395ed8b4b311d27af17
HeeYeon-Ko <rhgmldus6@gmail.com>
2025-11-01 21:45:28 +0900

    feat: 구입금액 입력 함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int money = inputAmount();
+        int count = money / 1000;
+
+        Lotto.createLottos(count);
+
+
     }
+
+    //구입금액 입력 함수
+    private static int inputAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = Integer.parseInt(Console.readLine());
+
+        if (money < 1000 || money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.");
+        }
+        return money;
+    }
+
+    //당첨 번호 + 보너스 번호 입력 함수
+    //private static List<Integer> inputWinningNumbersWithBonus(){}
 }
